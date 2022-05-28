947e9b3d53b651de89e1686b7fc9da8ec3d7f2b0
ryu <ryu000407@gmail.com>
2022-05-28 19:11:06 +0900

    update: add prettier and husky and update eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    // 'prefer-const': 'off'
+  },
+};
